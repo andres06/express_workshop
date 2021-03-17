8b43874cda0285b4814dcc1b29c47df13928df75
andres06 <andres06.acm@gmail.com>
2021-03-16 18:46:11 -0600

    initial commit

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const  express = require('express');
 const { restart } = require('nodemon');
 const app = express();
@@ -40,4 +41,48 @@ app.get('/pokemon/:name', (req, res, next) => {
 
 app.listen(process.env.PORT || 3000, ()  => {
 console.log("Server is running...");
+=======
+const  express = require('express');
+const { restart } = require('nodemon');
+const app = express();
+const { pokemon } = require('./pokedex.json');
+
+app.get("/", (req, res, next) => {
+    res.status(200);
+    res.send("Bienvenido al Pokedex");
+});
+
+app.get('/pokemon/all', (req, res, next) => {
+        res.status(200);
+        res.send(pokemon);
+});
+
+app.get('/pokemon/:id([0-9]{1,3})', (req, res, next) =>{
+    const id = req.params.id - 1;
+    if (id >= 0 && id <= 150) {
+        res.status(200);
+        res.send(pokemon[req.params.id - 1]);
+    }
+    else{
+       res.status(404);
+       res.send("Pokemon no encontrado");
+    }
+});
+
+app.get('/pokemon/:name', (req, res, next) => { 
+    const name  = req.params.name;
+    for(i = 0; i < pokemon.length; i++) {
+        if(pokemon[i].name == name) {
+            res.status(200);
+            res.send(pokemon[i]);
+        }
+    }
+    res.status(404);
+    res.send("Pokemon no encontrado");
+
+});
+
+app.listen(process.env.PORT || 3000, ()  => {
+console.log("Server is running...");
+>>>>>>> 3bb3fcb (initial commit)
 });
