144e1c5aa77cea91f8cf3950320d8a84e78ff752
andres06 <andres06.acm@gmail.com>
2021-04-13 19:12:36 -0500

    initial commit

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -2,8 +2,22 @@ const express = require ('express');
 const pokemon = express.Router();
 const db = require('../config/database');
 
-pokemon.post("/", (req, res, next) => {
-    return res.status(200).json(req.body);
+pokemon.post("/", async (req, res, next) => {
+    const { pok_name, pok_height, pok_weight, pok_base_experience } = req.body; 
+    
+    if(pok_name && pok_height && pok_weight && pok_base_experience ) {
+        let query = "INSERT INTO pokemon(pok_name, pok_height, pok_weight, pok_base_experience)";
+        query += ` VALUES ('${pok_name}', ${pok_height}, ${pok_weight}, ${pok_base_experience})`;
+        const rows = await db.query(query);
+        
+        if(rows.affectedRows == 1){
+            return res.status(201).json({ code: 201, message: "Pokemon insertado correctamente "});
+        }
+    
+        return res.status(500).json({code: 500, message: "Ocurrio un error"});;
+    }
+     return res.status(500).json({ code: 500, message: "Campos incompletos"});
+
 });
 
 pokemon.get('/', async (req, res, next) => {
@@ -15,7 +29,7 @@ pokemon.get('/:id([0-9]{1,3})', async (req, res, next) =>{
     const id = req.params.id;
    if (id >= 1 && id <= 722) {
     const pkmn = await db.query("SELECT * FROM pokemon WHERE pok_id=" + id + ";");
-      return res.status(200).json({ code: 1, message: pkmn});
+      return res.status(200).json({ code: 200, message: pkmn});
    } 
     return res.status(404).send({ code: 404, message: "Pokemon no encontrado" });
 });
@@ -25,7 +39,7 @@ pokemon.get('/:name([A-Za-z]+)', async (req, res, next) => {
     const pkmn = await db.query("SELECT * FROM pokemon WHERE pok_name=" + name + ";");
      return (p.name.toUpperCase() == name.toUpperCase()) && p; 
     if (pkmn.length > 0) {
-       return res.status(200).json({code: 1,  message: pkmn });
+       return res.status(200).json({code: 200,  message: pkmn });
     }
     return res.status(404).send({code: 404, message: "Pokemon no encontrado" });
 });
